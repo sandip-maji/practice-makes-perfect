ffb288c59a7b3a64013a2455833fe7d8b50c47de
Biraj Kumar Halder <36566108+BirajKumarHalder@users.noreply.github.com>
2021-08-17 13:02:23 +0530

    private method mock

--- Junit-powermock/src/test/java/com/org/service/CommonServiceTest.java
+++ Junit-powermock/src/test/java/com/org/service/CommonServiceTest.java
@@ -1,5 +1,6 @@
 package com.org.service;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 import org.junit.Test;
@@ -8,30 +9,22 @@ import org.mockito.InjectMocks;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
-import org.springframework.test.util.ReflectionTestUtils;
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest({CommonService.class})
+@PrepareForTest({ CommonService.class })
 public class CommonServiceTest {
-	
+
 	@InjectMocks
 	CommonService commonService;
-	
+
 	@Test
-	public void getServiceDetailsTest() throws Exception {
-		
-		//String string = ReflectionTestUtils.invokeMethod(commonService, "getMsg");
-		CommonService spy = PowerMockito.spy(commonService);
-		
-		PowerMockito.doReturn("mock").when(spy, "getMsg");
-		
-		String str = spy.getServiceDetails();
-		
-		String  result = commonService.getServiceDetails();
-		
+	public void testGetServiceDetails() throws Exception {
+		String mockedPrivateResponse = "This is mocked response";
+		CommonService commonServiceSpy = PowerMockito.spy(commonService);
+		PowerMockito.when(commonServiceSpy, "getMsg").thenReturn(mockedPrivateResponse);
+		String result = commonServiceSpy.getServiceDetails();
 		assertNotNull(result);
-		
+		assertEquals(mockedPrivateResponse, result);
 	}
-	
 
 }
